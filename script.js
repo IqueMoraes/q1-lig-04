a50bd90e8615244a8dfa849ac832fe10583c7734
glen1986 <escobar.glen@gmail.com>
2021-08-09 17:03:47 -0300

    idFilerasEColumnas

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function createDiv(container,className){
         container.appendChild(celula);
 };
 
+
 function createLayout(){
     for(let row in layout){
         const container = document.createElement('div');
@@ -29,6 +30,8 @@ function createLayout(){
             let print = layout[row][column];
             if(print === 'l'){
                 createDiv(container,'espaco');
+
+               // container.id="column"
             }
         }
     }
@@ -36,11 +39,66 @@ function createLayout(){
 createLayout();
 //Funções 
 
+const click = document.addEventListener("click", (event) =>{
+    let keyName = event.target;
+
+    console.log(keyName)
+})
+/*
+ asignar id pra criar columnas
+ */
+/*
+ listener de click
+ */
+const lineas = () =>{
+    const fila = document.querySelectorAll(".container");
+    for(let i = 0; i < fila.length; i++){
+        fila[i] = [];
+        fila[i].id = "filera"+(i+1);
+    }
+     
+}
+lineas();
+
+const columnas = () =>{
+    const linea1 = document.getElementById("filera1");
+    const linea2 = document.getElementById("filera2");
+    const linea3 = document.getElementById("filera3");
+    const linea4 = document.getElementById("filera4");
+    const linea5 = document.getElementById("filera5");
+    const linea6 = document.getElementById("filera6");
+    let materia1 = linea1.childNodes;
+    for(let i = 0; i < materia1.length; i ++){
+        materia1[i].id = "columna"+ (i+1);
+    }
+ let materia2 = linea2.childNodes;
+    for(let i = 0; i < materia2.length; i ++){
+        materia2[i].id = "columna"+ (i+1);
+    }
+ let materia3 = linea3.childNodes;
+    for(let i = 0; i < materia3.length; i ++){
+        materia3[i].id = "columna"+ (i+1);
+    }
+ let materia4 = linea4.childNodes;
+    for(let i = 0; i < materia4.length; i ++){
+        materia4[i].id = "columna"+ (i+1);
+    }
+ let materia5 = linea5.childNodes;
+    for(let i = 0; i < materia5.length; i ++){
+        materia5[i].id = "columna"+ (i+1);
+    }
+ let materia6 = linea6.childNodes;
+    for(let i = 0; i < materia6.length; i ++){
+        materia6[i].id = "columna"+ (i+1);
+    }
+}
+
+columnas();
 //Event Listener
 const div = document.querySelector('.espaco');
 let change = true;
 
-div.addEventListener('click', () =>{
+/*div.addEventListener('click', () =>{
 
     if(change === true){
       const red = document.createElement('div');
@@ -58,4 +116,5 @@ div.addEventListener('click', () =>{
         change = true;
     }
 });
+*/
 //Event Listener
